be316556cbf3ca9a4cf33b3893967bb98b070459
Lxiao <Lxiao@atguigu.com>
2023-06-29 10:41:34 +0800

    修改备案信息

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,9 +1,20 @@
 <template>
   <footer class="copyright">
-    <p>Copyright © 2023 <span>咖啡不加糖</span> All Rights Reservea</p>
+    <div class="beian">
+      <p>Copyright ©2023 by @ <span>MR XIAO</span> . All rights reserved</p>
+      <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=36082802000136"
+        style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img
+          src="../../assets/images/备案图标.png" style="float:left;" />
+
+        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px;font-size: 16px; color:#939393;">赣公网安备
+          36082802000136号
+        </p>
+      </a>
+    </div>
+
     <div class="copyright-iframe" style="margin-top: 10px">
-      <!-- <iframe name="weather_inc" src="http://i.tianqi.com/?c=code&amp;id=1" height="25" frameborder="0" marginwidth="0"
-        marginheight="0" scrolling="no" style="max-width: 1200px"></iframe> -->
+      <iframe name="weather_inc" src="http://i.tianqi.com/?c=code&amp;id=1" height="25" frameborder="0" marginwidth="0"
+        marginheight="0" scrolling="no"></iframe>
     </div>
   </footer>
 </template>
@@ -27,8 +38,22 @@
     }
   }
 
-  span {
-    color: #f03c2e;
+  .beian {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &>p {
+      margin-right: 20px;
+
+      span {
+        color: #f03c2e;
+      }
+    }
   }
+
+  // span {
+  //   color: #f03c2e;
+  // }
 }
 </style>
